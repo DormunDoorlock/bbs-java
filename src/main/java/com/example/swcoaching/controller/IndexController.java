102c94a8450ee7140b329ea02c420a2bd7e2ca52
DormunDoorlock <43779340+DormunDoorlock@users.noreply.github.com>
2022-07-07 08:01:56 +0900

    Board 선택 부분 완료

---
+++ src/main/java/com/example/swcoaching/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.example.swcoaching.controller;
+
+import com.example.swcoaching.board.Board;
+import com.example.swcoaching.board.BoardService;
+import com.example.swcoaching.board.jpa.BoardEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping
+public class IndexController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("boardList",boardService.scanBoardList());
+        return "index";
+    }
+}
